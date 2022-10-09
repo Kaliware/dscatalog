58dcb7b48178371f742f81728cf1e8c9c5004330
Lucas Lourenço <kaliware.player@gmail.com>
2022-10-09 18:23:47 -0300

    Pagination

--- backend/src/main/java/com/kaliware/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/com/kaliware/dscatalog/controllers/CategoryController.java
@@ -4,6 +4,9 @@ import com.kaliware.dscatalog.dto.CategoryDTO;
 import com.kaliware.dscatalog.entities.Category;
 import com.kaliware.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -20,8 +23,18 @@ public class CategoryController{
   CategoryService service;
 
   @GetMapping
-  public ResponseEntity<List<CategoryDTO>> findAll(){
-    return ResponseEntity.ok().body(service.findAll());
+  public ResponseEntity<Page<CategoryDTO>> findAll(
+          @RequestParam(value = "page", defaultValue = "0") Integer page,
+          @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+          @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+          @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+          ){
+
+    PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction) ,orderBy);
+
+    Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+
+    return ResponseEntity.ok().body(list);
   }
 
   @GetMapping(value = "/{id}")
