0c4bcba6024e2e2b0e390b55b738cd823471365d
Lucas Lourenço <kaliware.player@gmail.com>
2022-10-08 21:47:54 -0300

    CategoryController

---
+++ backend/src/main/java/com/kaliware/dscatalog/entities/Category.java
@@ -0,0 +1,49 @@
+package com.kaliware.dscatalog.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable{
+
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+  private String name;
+
+  public Category(){
+  }
+
+  public Category(Long id, String name){
+    this.id = id;
+    this.name = name;
+  }
+
+  public Long getId(){
+    return id;
+  }
+
+  public void setId(Long id){
+    this.id = id;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  @Override
+  public boolean equals(Object obj){
+    if(this == obj) return true;
+    if(obj == null || getClass() != obj.getClass()) return false;
+    Category category = (Category) obj;
+    return Objects.equals(getId(), category.getId());
+  }
+
+  @Override
+  public int hashCode(){
+    return Objects.hash(getId());
+  }
+}
